6a075563b4a9b3c8ea39127e16cfad2a0eb4709d
shoichiimamura <imamura@c-fo.com>
2018-12-30 14:56:57 +0900

    :sparkles: Parse secret.yaml

--- cmd/root.go
+++ cmd/root.go
@@ -3,8 +3,13 @@ package cmd
 import (
 	"fmt"
 	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
+	"io"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/util/yaml"
+	"k8s.io/kubernetes/pkg/apis/core"
+	"log"
 	"os"
+	"path/filepath"
 )
 
 // 共通フラグ用の変数
@@ -12,33 +17,35 @@ import (
 // var name1 string
 
 // 読み込む設定の型
-type Config struct {
-	ApplicationName string
-	Debug           bool
-}
+// type Config struct {
+// 	ApplicationName string
+// 	Debug           bool
+// }
 
 // 読み込む設定ファイル名
-var configFile string
+// var configFile string
 
 // 読み込んだ設定ファイルの構造体
-var config Config
+// var config Config
 
 var rootCmd = &cobra.Command{
-	Use: "app",
-	Run: func(c *cobra.Command, args []string) {
-		// 受け取った設定ファイル名と設定の内容を表示
-		fmt.Printf("configFile: %s\nconfig: %#v", configFile, config)
-	},
+	Use: "kubase",
+	Run: root,
 }
+
+// ファイル名
+var fileName string
 
 // ファイル読み込みのタイミングでフラグを定義する
 func init() {
+	// -d filename でdecodeする
+	rootCmd.Flags().StringVarP(&fileName, "decode", "d", "", "decode base64 file")
 	// 設定ファイル名をフラグで受け取る
-	rootCmd.PersistentFlags().StringVarP(&configFile, "config", "c", "./config.yaml", "config file name")
+	// rootCmd.PersistentFlags().StringVarP(&configFile, "config", "c", "./config.yaml", "config file name")
 
 	// 設定ファイルの ApplicationName 項目をフラグで上書きする
-	rootCmd.PersistentFlags().String("name", "", "application name")
-	viper.BindPFlag("ApplicationName", rootCmd.PersistentFlags().Lookup("name"))
+	// rootCmd.PersistentFlags().String("name", "", "application name")
+	// viper.BindPFlag("ApplicationName", rootCmd.PersistentFlags().Lookup("name"))
 
 	// cobra.Command 実行前の初期化処理を定義する。
 	// rootCmd.Execute > コマンドライン引数の処理 > cobra.OnInitialize > rootCmd.Run という順に実行されるので、
@@ -46,22 +53,22 @@ func init() {
 	cobra.OnInitialize(func() {
 
 		// 設定ファイル名を viper に定義する
-		viper.SetConfigFile(configFile)
+		// viper.SetConfigFile(configFile)
 
 		// env
-		viper.AutomaticEnv()
+		// viper.AutomaticEnv()
 
 		// 設定ファイルを読み込む
-		if err := viper.ReadInConfig(); err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
-
-		// 設定ファイルの内容を構造体にコピーする
-		if err := viper.Unmarshal(&config); err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
+		// if err := viper.ReadInConfig(); err != nil {
+		// 	fmt.Println(err)
+		// 	os.Exit(1)
+		// }
+		//
+		// // 設定ファイルの内容を構造体にコピーする
+		// if err := viper.Unmarshal(&config); err != nil {
+		// 	fmt.Println(err)
+		// 	os.Exit(1)
+		// }
 	})
 
 	// コマンド共通のフラグを定義
@@ -79,6 +86,31 @@ func init() {
 	// 第3引数: デフォルト値
 	// 第4引数: 説明
 	// rootCmd.PersistentFlags().StringP("name2", "n", "default", "your name2")
+}
+
+type Secret struct {
+	core.Secret
+	Data map[string][]byte
+}
+
+func root(c *cobra.Command, args []string) {
+	fmt.Printf("fileName: %v\n", fileName)
+	fmt.Printf("ext: %v\n", filepath.Ext(fileName))
+	f, err := os.Open("config.yaml")
+	if err != nil {
+		log.Fatal(err)
+	}
+	d := yaml.NewYAMLOrJSONDecoder(f, 4096)
+	for {
+		ext := runtime.RawExtension{}
+		if err := d.Decode(&ext); err != nil {
+			if err == io.EOF {
+				break
+			}
+			log.Fatal(err)
+		}
+		fmt.Println("raw: ", string(ext.Raw))
+	}
 }
 
 func Execute() {
--- cmd/sub.go
+++ cmd/sub.go
@@ -1,29 +1,24 @@
 package cmd
 
-import (
-	"github.com/spf13/cobra"
-	"fmt"
-)
-
 // サブコマンドの定義
-var subCmd = &cobra.Command{
-	Use: "sub",
-	Run: func(c *cobra.Command, args []string) {
-		if name, err := c.PersistentFlags().GetString("name"); err == nil {
-			// 共通フラグ debug をサブコマンドからも利用できる
-			// if debug {
-			// 	log.Println("name:", name)
-			// } else {
-			// 	fmt.Println("name:", name)
-			// }
-			fmt.Println("name:", name)
-		}
-	},
-}
+// var subCmd = &cobra.Command{
+// 	Use: "sub",
+// 	Run: func(c *cobra.Command, args []string) {
+// 		if name, err := c.PersistentFlags().GetString("name"); err == nil {
+// 			// 共通フラグ debug をサブコマンドからも利用できる
+// 			// if debug {
+// 			// 	log.Println("name:", name)
+// 			// } else {
+// 			// 	fmt.Println("name:", name)
+// 			// }
+// 			fmt.Println("name:", name)
+// 		}
+// 	},
+// }
 
 func init() {
 	// サブコマンドのフラグ定義
-	subCmd.PersistentFlags().String("name", "john", "sub command string flag test")
+	// subCmd.PersistentFlags().String("name", "john", "sub command string flag test")
 	// サブコマンドをルートコマンドに登録
-	rootCmd.AddCommand(subCmd)
+	// rootCmd.AddCommand(subCmd)
 }
